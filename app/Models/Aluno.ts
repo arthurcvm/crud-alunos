33ed93cf8298bb1924f65270c5dd252106c99ad9
Arthur Cezar <arthurcvm@hotmail.com>
2021-08-19 22:08:03 -0300

    feat: implements model

--- app/Models/Aluno.ts
+++ app/Models/Aluno.ts
@@ -1,9 +1,25 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { SexoEnum } from 'Contracts/enums/SexoEnum'
 
 export default class Aluno extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public matricula: string
+
+  @column()
+  public nome: string
+
+  @column()
+  public sexo: SexoEnum
+
+  @column()
+  public idade: number
+
+  @column()
+  public cidade_natal: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
